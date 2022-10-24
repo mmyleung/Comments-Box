c17031f032e4a52ffe8ca507c3552cc3a7529754
mmyleung <63979415+mmyleung@users.noreply.github.com>
2022-10-24 16:38:57 +0100

    Added character count and red border if exceeded

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 console.log('Hello!');
 const commentBox = document.getElementById('comment');
-commentBox.
+const count = document.getElementById('count');
+commentBox.onkeyup = function() {
+count.innerHTML = commentBox.value.length;
+if (count.innerHTML > 140) {
+commentBox.style.borderColor = 'red';
+} else {
+    commentBox.style.borderColor = 'blue';
+}
+}
