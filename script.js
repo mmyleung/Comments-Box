9af3d0bdbf69a419ce82878546b22d80d2d4ca9d
mmyleung <63979415+mmyleung@users.noreply.github.com>
2022-11-22 21:03:43 +0700

    prevent thank you message from repeated on submit button click

--- script.js
+++ script.js
@@ -10,13 +10,18 @@ commentBox.style.borderColor = 'red';
 }
 }
 const form = document.getElementById('comment-form');
-const submittedComment = document.getElementById('submitted-comment');
+const submittedCommentContainer = document.getElementById('submitted-comment');
+submittedCommentContainer.style.display = 'none';
+
 function handleForm(event) {
     if (count.innerHTML >140) {
         event.preventDefault();
     } else {
     event.preventDefault();
-    submittedComment.innerHTML += ("Thank you for submitting your comment: <br>" + commentBox.value);
+    submittedCommentContainer.style.display = 'block';
+    const comments = document.createElement('div');
+    submittedCommentContainer.appendChild(comments);
+    comments.innerHTML += commentBox.value;
     }
 }
 form.addEventListener('submit',handleForm);
